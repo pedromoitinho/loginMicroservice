43c9852063ff20f69e679b7d52a7ec6f05aeae9a
Pedro <pedrohenriquemoitinho.prado@ucsal.edu.br>
2025-05-26 15:50:15 -0300

    persistent db

--- src/main/java/com/codecraft/auth/AuthApplication.java
+++ src/main/java/com/codecraft/auth/AuthApplication.java
@@ -1,13 +1,18 @@
 package com.codecraft.auth;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AuthApplication {
+    public static void main(String[] args) {
+        // Carrega o arquivo .env
+        Dotenv dotenv = Dotenv.configure().load();
+        System.setProperty("DB_URL", dotenv.get("DB_URL"));
+        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
 
-	public static void main(String[] args) {
-		SpringApplication.run(AuthApplication.class, args);
-	}
-
+        SpringApplication.run(AuthApplication.class, args);
+    }
 }
